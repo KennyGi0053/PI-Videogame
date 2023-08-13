ac235c8a9af58b45a09913bcdf9b21026c8fed63
KGabriel Perez <121875673+KennyGi0053@users.noreply.github.com>
2023-08-13 12:05:49 -0300

    db

--- api/src/db.js
+++ api/src/db.js
@@ -6,8 +6,8 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgresql://postgres:sBzz6LxxEezR8KVcko8e@containers-us-west-157.railway.app:7320/railway`, {
-//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+//const sequelize = new Sequelize(`postgresql://postgres:sBzz6LxxEezR8KVcko8e@containers-us-west-157.railway.app:7320/railway`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
